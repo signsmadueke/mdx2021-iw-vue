e032d6263fcadf889d346879a11bf7e6eac1a4fc
signsmadueke <signsmadueke1@gmail.com>
2021-11-23 15:59:13 +0100

    More JS

--- js/script.js
+++ js/script.js
@@ -40,5 +40,78 @@ var webstore = new Vue({
         canAddToCart(lesson) {
             return lesson.spaces > this.cartCount(lesson);
         },
+
+        cartCheckout() {
+            let name = document.getElementById("name")
+            let phone = document.getElementById("phone")
+            var nameRegEx = /[a-zA-Z ]*$/
+            var phoneRegEx = /(0|[1-9][0-9]*)$/
+            if (name.value.match(nameRegEx) && phone.value.match(phoneRegEx)) {
+                alert('Your order has been submitted!');
+                window.location.reload();
+                return true;
+            } else {
+                alert('The ʼNameʼ must be letters only and the ʼPhoneʼ must be numbers only');
+                return false;
+            }
+        },
+
+        canCheckout() {
+            this.checkout.name = this.checkout.name.replace(/(0|[1-9][0-9]*)$/g, '')
+            this.checkout.phone = this.checkout.phone.replace(/[a-zA-Z ]*$/g, '')
+        }
+    },
+    computed: {
+
+        sorted() {
+            let orderLessons = this.lessons;
+
+            orderLessons = orderLessons.sort((x, y) => {
+                if (this.sortBy == "subject") {
+                    let ax = x.subject.toLowerCase(),
+                        ay = y.subject.toLowerCase();
+
+                    if (ax < ay) {
+                        return -1
+                    }
+                    if (ax > ay) {
+                        return 1
+                    }
+                } else if (this.sortBy == 'location') {
+                    let ax = x.location.toLowerCase(),
+                        ay = y.location.toLowerCase();
+
+                    if (ax < ay) {
+                        return -1
+                    }
+                    if (ax > ay) {
+                        return 1
+                    }
+                }
+                return 0
+            })
+
+            if (this.sortBy == 'price') {
+                orderLessons = orderLessons.sort((x, y) => {
+                    return x.price - y.price
+                })
+            }
+            if (this.sortBy == 'availability') {
+                orderLessons = orderLessons.sort((x, y) => {
+                    return x.spaces - y.spaces
+                })
+            }
+            if (this.orderBy !== "ascending") {
+                orderLessons.reverse()
+            }
+            if (this.searchTerm != '' && this.searchTerm) {
+                orderLessons = orderLessons.filter((item) => {
+                    return item.subject
+                        .toUpperCase()
+                        .includes(this.searchTerm.toUpperCase())
+                })
+            }
+            return orderLessons
+        },
     }
 })
